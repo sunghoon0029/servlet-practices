9cf0d8b1a2201331c5a4431dbc9eb7dce4bf18ab
tjdgns0029 <whtjdgns0029@naver.com>
2022-11-11 17:53:46 +0900

    jstlel

---
+++ guestbook02/src/main/java/com/bitacademy/guestbook/controller/GuestbookController.java
@@ -0,0 +1,49 @@
+package com.bitacademy.guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.guestbook.dao.GuestbookDao;
+import com.bitacademy.guestbook.vo.GuestbookVo;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/deleteform.jsp");
+			rd.forward(request, response);
+		} else if("delete".equals(action)) {
+			String password = request.getParameter("password");
+			String sno = request.getParameter("no");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setPassword(password);
+			
+			new GuestbookDao().deleteByNoAndPassword(no, password);
+			
+			response.sendRedirect(request.getContextPath() + "/guestbook");
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			
+			request.setAttribute("list", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ guestbook02/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,53 @@
+<%@page import="com.bitacademy.guestbook.dao.GuestbookDao"%>
+<%@page import="java.util.List"%>
+<%@page import="com.bitacademy.guestbook.vo.GuestbookVo"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	List<GuestbookVo> list = (List<GuestbookVo>)request.getAttribute("list");
+%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록</title>
+</head>
+<body>
+	<form action="add.jsp" method="post">
+	<table border=1 width=500>
+		<tr>
+			<td>이름</td><td><input type="text" name="name"></td>
+			<td>비밀번호</td><td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td colspan=4><textarea name="message" cols=60 rows=5></textarea></td>
+		</tr>
+		<tr>
+			<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
+		</tr>
+	</table>
+	</form>
+	<br>
+	<%
+		int count = list.size();
+		for(GuestbookVo vo : list) {
+	%>
+		<table width=510 border=1>
+			<tr>
+				<td>[<%=count-- %>]</td>
+				<td><%=vo.getName() %></td>
+				<td><%=vo.getRegDate() %></td>
+				<td>
+					<a href="deleteform.jsp?no=<%=vo.getNo() %>">삭제</a>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=4>
+					<%=vo.getContents().replaceAll("\n", "<br>") %>
+				</td>
+			</tr>
+		</table>
+		<br>
+	<%
+		}
+	%>
+</body>
+</html>
